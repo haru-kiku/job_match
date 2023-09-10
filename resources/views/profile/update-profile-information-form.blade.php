2a3f7509416096c726562aea02ab66fc08185a49
haru-kiku <haru11250321@gmail.com>
2023-09-10 17:48:18 +0900

    求人マッチングサイト完了

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -1,19 +1,21 @@
 <x-form-section submit="updateProfileInformation">
     <x-slot name="title">
-        {{ __('会社プロフィール情報') }}
+        {{ __('Profile Information') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('会社プロフィール情報を更新する。') }}
+        {{ __('Update your account\'s profile information and email address.') }}
     </x-slot>
 
     <x-slot name="form">
-        <!-- Company Profile Photo -->
+        <!-- Profile Photo -->
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-            <div x-data="{ photoName: null, photoPreview: null }" class="col-span-6 sm:col-span-4">
-                <!-- Company Profile Photo File Input -->
-                <input type="file" class="hidden" wire:model="photo" x-ref="photo"
-                    x-on:change="
+            <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
+                <!-- Profile Photo File Input -->
+                <input type="file" class="hidden"
+                            wire:model.live="photo"
+                            x-ref="photo"
+                            x-on:change="
                                     photoName = $refs.photo.files[0].name;
                                     const reader = new FileReader();
                                     reader.onload = (e) => {
@@ -24,16 +26,15 @@
 
                 <x-label for="photo" value="{{ __('Photo') }}" />
 
-                <!-- Current Company Profile Photo -->
+                <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->company->profile_photo_url }}" alt="{{ $this->user->company->name }}"
-                        class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
-                <!-- New Company Profile Photo Preview -->
+                <!-- New Profile Photo Preview -->
                 <div class="mt-2" x-show="photoPreview" style="display: none;">
                     <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                        x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                          x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
                     </span>
                 </div>
 
@@ -41,31 +42,51 @@
                     {{ __('Select A New Photo') }}
                 </x-secondary-button>
 
-                @if ($this->user->company->profile_photo_path)
+                @if ($this->user->profile_photo_path)
                     <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
                         {{ __('Remove Photo') }}
                     </x-secondary-button>
                 @endif
 
-                <x-input-error for="company_photo" class="mt-2" />
+                <x-input-error for="photo" class="mt-2" />
             </div>
         @endif
 
-        <!-- Company Name -->
+        <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="company_name" value="{{ __('会社名') }}" />
-            <x-input name="company_name" id="company_name" type="text" class="mt-1 block w-full"
-                wire:model.defer="state.company.name" autocomplete="company_name" />
-            <x-input-error for="company.name" class="mt-2" />
+            <x-label for="name" value="{{ __('Name') }}" />
+            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
+            <x-input-error for="name" class="mt-2" />
         </div>
 
-        <!-- Company Profile -->
+        <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="company_profile" value="{{ __('会社紹介') }}" />
-            <textarea name="company_profile" id="company_profile" cols="30" rows="5"
-                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full"
-                required wire:model.defer="state.company.profile"></textarea>
-            <x-input-error for="company.profile" class="mt-2" />
+            <x-label for="email" value="{{ __('Email') }}" />
+            <x-input id="email" type="email" class="mt-1 block w-full" wire:model="state.email" required autocomplete="username" />
+            <x-input-error for="email" class="mt-2" />
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) && ! $this->user->hasVerifiedEmail())
+                <p class="text-sm mt-2">
+                    {{ __('Your email address is unverified.') }}
+
+                    <button type="button" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click.prevent="sendEmailVerification">
+                        {{ __('Click here to re-send the verification email.') }}
+                    </button>
+                </p>
+
+                @if ($this->verificationLinkSent)
+                    <p class="mt-2 font-medium text-sm text-green-600">
+                        {{ __('A new verification link has been sent to your email address.') }}
+                    </p>
+                @endif
+            @endif
+        </div>
+
+        <!-- Profile -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="profile" value="{{ __('自己紹介') }}" />
+            <textarea name="profile" id="profile" cols="30" rows="5" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full" wire:model.defer="state.profile" ></textarea>
+            <x-input-error for="profile" class="mt-2" />
         </div>
     </x-slot>
 
